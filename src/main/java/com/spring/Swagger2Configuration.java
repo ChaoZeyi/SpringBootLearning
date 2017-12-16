a97ac2dfad7786507329dbe1bf1d20d5fae8123e
ChaoZeyi <chaozeyi1995@sina.com>
2017-12-16 20:25:56 +0800

    Swagger2

---
+++ src/main/java/com/spring/Swagger2Configuration.java
@@ -0,0 +1,46 @@
+package com.spring;
+
+import com.google.common.base.Predicate;
+import org.springframework.boot.autoconfigure.web.BasicErrorController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author cherry
+ * @date 2017/12/16 16:57
+ */
+@Configuration
+@EnableSwagger2
+public class Swagger2Configuration {
+    @Bean
+    public Docket createRestApi() {
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.spring.controller"))
+//                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot中使用Swagger2构建RESTful APIs")
+                .description("第一个Spring Boot项目")
+                .contact("cherry")
+                .version("1.0")
+                .build();
+    }
+}
