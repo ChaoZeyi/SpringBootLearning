52171957031dc3ab039e06178ee55a96b086cb30
ChaoZeyi <chaozeyi1995@sina.com>
2017-12-16 20:25:10 +0800

    main

--- src/main/java/com/spring/CherryApplication.java
+++ src/main/java/com/spring/CherryApplication.java
@@ -2,8 +2,11 @@ package com.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class CherryApplication {
 
 	public static void main(String[] args) {
