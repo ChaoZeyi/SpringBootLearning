5ed43fcecf78b9d8fc521ccbb9c10a8e346c3dc4
ChaoZeyi <chaozeyi1995@sina.com>
2017-12-17 17:50:43 +0800

    main

--- src/main/java/com/spring/CherryApplication.java
+++ src/main/java/com/spring/CherryApplication.java
@@ -1,5 +1,6 @@
 package com.spring;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -7,6 +8,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@MapperScan("com.spring.mapper")
 public class CherryApplication {
 
 	public static void main(String[] args) {
