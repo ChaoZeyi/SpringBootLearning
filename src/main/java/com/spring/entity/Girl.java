bb21c933e5d580a340df2ca098f20cfaba0b437e
ChaoZeyi <chaozeyi1995@sina.com>
2017-12-17 17:50:07 +0800

    entity

---
+++ src/main/java/com/spring/entity/Girl.java
@@ -0,0 +1,37 @@
+package com.spring.entity;
+
+
+/**
+ * @author cherry
+ * @date 2017/12/16 21:54
+ */
+public class Girl {
+
+    private Integer id;
+    private String name;
+    private Integer age;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
