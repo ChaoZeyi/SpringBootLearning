19c5bb69bed4d0b64002cf9da0d1e943008450c2
ChaoZeyi <chaozeyi1995@sina.com>
2017-12-17 17:49:29 +0800

    vo

---
+++ src/main/java/com/spring/vo/AddGirlVo.java
@@ -0,0 +1,28 @@
+package com.spring.vo;
+
+/**
+ * @author cherry
+ * @date 2017/12/17 17:05
+ */
+public class AddGirlVo {
+
+    private String name;
+
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
